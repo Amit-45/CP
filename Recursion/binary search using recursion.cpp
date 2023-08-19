0361001dac299eb030dbe27a7e9ebf0db0f96805
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2023-08-19 05:55:59 +0530

    Create binary search using recursion.cpp

---
+++ Recursion/binary search using recursion.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+bool binarySearchUsingRecursion(int low, int high, int key, int arr[])
+{
+    // base case
+    if (low <= high)
+    {
+        int mid = low + (high - low) / 2; // Calculate the middle index
+
+        if (arr[mid] == key)
+        {
+            return true; // Key found at middle index
+        }
+        else if (arr[mid] < key)
+        {
+            return binarySearchUsingRecursion(mid + 1, high, key, arr); // Search in the right half
+        }
+        else
+        {
+            return binarySearchUsingRecursion(low, mid - 1, key, arr); // Search in the left half
+        }
+    }
+
+    return false; // Key not found
+}
+
+int main()
+{
+    int arr[] = {1, 9, 3, 4, 5, 6, 7, 8, 9, 10};
+    int n = sizeof(arr) / sizeof(int);
+    int key = 200; // Key to search for
+
+    bool found = binarySearchUsingRecursion(0, n - 1, key, arr);
+
+    if (found)
+    {
+        std::cout << "Key found!" << std::endl;
+    }
+    else
+    {
+        std::cout << "Key not found." << std::endl;
+    }
+
+    return 0;
+}
