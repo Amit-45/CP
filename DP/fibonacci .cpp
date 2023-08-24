77cb9df508cb322a613386ff910375d063acf90a
Amit Patnaik <77204104+Amit-45@users.noreply.github.com>
2023-08-24 13:35:02 +0530

    Update fibonacci .cpp

--- DP/fibonacci .cpp
+++ DP/fibonacci .cpp
@@ -1,4 +1,24 @@
+O(2^n) : Recursive appproach
 
+
+int fib(int n)
+{
+    if(n<=1) return n;
+
+    return fib(n-1) + fib(n-2);
+}
+void solve()
+{
+    int n;
+    cin>>n;
+
+    cout<<fib(n);
+}
+
+
+
+
+O(n) : DP approach
 int fib(int n,vector<int> &dp)
 {
     if(n<=1) return n;
@@ -14,4 +34,32 @@ void solve()
   vector<int> dp(n+1,-1);
   cout<<fib(n, dp);
 
+}
+
+
+O(n) : Space optimised approach
+
+int fib(int n)
+{
+    if(n<=1) return n;
+
+    int prev=0;
+    int curr=1;
+
+    for(int i=2;i<=n;i++)
+    {
+        int temp = curr;
+        curr = prev+temp;
+        prev=temp;
+        
+    }
+    return curr;
+    
+}
+
+void solve()
+{
+    int n;
+    cin>>n;
+    cout<<fib(n);
 }
